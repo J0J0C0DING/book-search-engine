45d5a23c51dfddfdf6073828e752793cc28dec84
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-09-21 20:54:54 -0500

    Added shcemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,46 @@
 // import gql tagged template function
 const { gql } = require('apollo-server-express');
 
+// create our typeDefs
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      authors: [String]!
+      description: String
+      title: String!
+      bookId: ID
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
